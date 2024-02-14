8148ac8dd47fdd2209593511670de08a1ab2eabe
Alexis Paz <alexismpaz@gmail.com>
2024-02-14 09:30:49 -0300

    Build: Header, Carousel, Banner

---
+++ src/components/Home/Welcome/Banner/BannerTitle.jsx
@@ -0,0 +1,24 @@
+export const BannerTitle = (props) => (
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        xmlSpace="preserve"
+        style={{
+            enableBackground: "new 0 0 1000 300",
+        }}
+        viewBox="0 0 1000 300"
+        {...props}
+    >
+        <text
+            style={{
+                letterSpacing: 15,
+                fontSize: 200,
+                fontFamily: "&quot",
+                fill: "#d35a24",
+                fontWeight: 600
+            }}
+            transform="translate(28.8 220.587)"
+        >
+            {"CATAVIA"}
+        </text>
+    </svg>
+);
